3d011322a958e5348fb3e919dce4b13100a340f8
sunny kyung <shkyung@lunit.io>
2021-02-03 02:25:31 +0900

    Sample #3 separate loading

--- src/lib/createRequestThunk.js
+++ src/lib/createRequestThunk.js
@@ -1,9 +1,12 @@
+import { startLoading, finishLoading } from '../modules/loading';
+
 export default function createRequestThunk(type, request) {
   const SUCCESS = `${type}_SUCCESS`;
   const FAILURE = `${type}_FAILURE`;
 
   return (params) => async (dispatch) => {
     dispatch({ type }); // 시작됨.
+    dispatch(startLoading(type))
 
     try {
       const response = await request(params);
@@ -11,12 +14,14 @@ export default function createRequestThunk(type, request) {
         type: SUCCESS,
         payload: response.data,
       }); //성공
+      dispatch(finishLoading(type))
     } catch (e) {
       dispatch({
         type: FAILURE,
         payload: e,
         error: true,
       });
+      dispatch(startLoading(type))
       throw e;
     }
   };
