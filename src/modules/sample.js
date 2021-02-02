f2d06d0bdc6b6bc2f50f069d40584087acda66ae
sunny kyung <shkyung@lunit.io>
2021-02-03 01:12:23 +0900

    Sample #2 refactor

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -1,5 +1,6 @@
 import { handleActions } from 'redux-actions';
 import * as api from '../lib/api';
+import createRequestThunk from "../lib/createRequestThunk";
 
 // 액션 타입을 선언합니다.
 // 한 요청당 세 개를 만들어야 합니다.
@@ -14,43 +15,8 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 // thunk 함수를 생성합니다.
 // thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치합니다.
-
-export const getPost = (id) => async (dispatch) => {
-  dispatch({ type: GET_POST }); // 요청을 시작한 것을 알림
-
-  try {
-    const response = await api.getPost(id);
-    dispatch({
-      type: GET_POST_SUCCESS,
-      payload: response.data,
-    });
-  } catch (e) {
-    dispatch({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true,
-    }); // 에러 발생
-    throw e;
-  }
-};
-
-export const getUsers = () => async (dispatch) => {
-  dispatch({ type: GET_USERS });
-  try {
-    const response = await api.getUsers();
-    dispatch({
-      type: GET_USERS_SUCCESS,
-      payload: response.data,
-    });
-  } catch (e) {
-    dispatch({
-      type: GET_USERS_FAILURE,
-      payload: e,
-      error: true,
-    });
-    throw e;
-  }
-};
+export const getPost = createRequestThunk(GET_POST, api.getPost)
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers)
 
 const initialState = {
   loading: {
