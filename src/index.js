c1302c157b3ab6add5e18d81d5bbb193f1733993
sunny kyung <shkyung@lunit.io>
2021-02-02 22:11:56 +0900

    tried ReduxThunk

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ import reportWebVitals from './reportWebVitals';
 import rootReducer from './modules';
 //import loggerMiddleware from './lib/loggerMiddleware';
 import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
 const logger = createLogger();
 
 //const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
